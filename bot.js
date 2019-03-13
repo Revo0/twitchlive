fc5cf09c011f2f48677b086d6f6d1a7fec4c65cf
Revo0 <47788817+Revo0@users.noreply.github.com>
2019-03-13 17:15:11 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,21 @@
 
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('token');
+
+
 const devs = ["460819539303727104"];
 const adminprefix = ["!"];
 client.on('message', message => {
