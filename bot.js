08e9feeb6833052d0c294d84e0f255051f0b0d1f
Revo0 <47788817+Revo0@users.noreply.github.com>
2019-03-24 11:25:11 +0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,9 @@
-
-
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
-  }
-});
-
-client.login('token');
-
 
 const devs = ["460819539303727104"];
 const adminprefix = ["!"];
@@ -49,4 +38,6 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {
   client.user.setAvatar(argresult);
     message.channel.send(Changing The Avatar To :${argresult} `);
 }
+});
+  }
 });
